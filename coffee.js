4feff7d0c21bbcb82619cc2389aebd5fa7a00878
Trinitra-tt <x2x@ya.ru>
2020-04-23 17:58:08 +0300

    Машина умеет выдавать монеты по 10 руб.

--- coffee.js
+++ coffee.js
@@ -3,6 +3,8 @@ let state = "waiting";
 
 let cupImg = document.querySelector(".coffee-cup img");
 let progressBar = document.querySelector(".progress-bar");
+let balanceInput = document.querySelector("input[placeholder='Баланс']");
+
 cupImg.onclick = takeCoffee;
 
 
@@ -13,7 +15,6 @@ function buyCoffee(name, price, element) {
   if (state != "waiting") { // ожидание - пока не приготовится кофе, новую кружку заказать нельзя!
     return;
   }
-  let balanceInput = document.querySelector("input[placeholder='Баланс']");
   
   if ( +balanceInput.value < price) {
     changeDisplayText("Недостаточно средств");
@@ -68,13 +69,115 @@ function changeDisplayText(text) {
   displayText.innerHTML = text;
 }
 
+//--------------КУПЮРЫ------------------------------------
+let bills = document.querySelectorAll('.bills img');
+for (let i = 0; i < bills.length; i++) {
+  bills[i].onmousedown = takeMoney; // выбирать купюру мышкой и тащить в купюроприёмник
+/*  bills[i].onmousedown = function (event) {
+    takeMoney(event);  ---- можно сделать выбор купюры так же через функцию обёртку, но не рекомендуется
+  }*/
+}
+function takeMoney(event) {
+  event.preventDefault(); //отключить браузерные события которые были назначены по умолчанию (default == по умолчанию. Сброс значений по умолчанию.)
+  let bill = event.target;
+  
+  bill.style.position = "absolute";
+  bill.style.transform = "rotate(90deg)"//перевернули купюры на 90 градусов
+  bill.style.margin = 0;
+  
+  let billCoords = bill.getBoundingClientRect(); // getBoundingClientRect - позволяет получить координаты и ширину-высоту объекта
+  let billWidth = billCoords.width;
+  let billHeight = billCoords.height;
+/*  console.log(event.clientX, event.clientY); //отображает положение курсора по оси х и по оси у.
+*/   
+  
+  bill.style.top = event.clientY - billWidth/2 + "px"; 
+  bill.style.left = event.clientX - billHeight/2 + "px";
+  
+  window.onmousemove = function(event) {
+    bill.style.top = event.clientY - billWidth/2 + "px"; 
+    bill.style.left = event.clientX - billHeight/2 + "px";
+  }
+  
+  bill.onmouseup = function() {  //отжать мышь от купюры
+    window.onmousemove = null;
+    if ( inAtm(bill) ) {
+      let billCost = +bill.getAttribute('cost');
+      balanceInput.value = + balanceInput.value + billCost;
+      bill.remove();
+    }
+  }
+  
+}
 
+function inAtm(bill) {
+  let atm = document.querySelector('.atm img'); // попадает ли курюра в АТМ
+  
+  let atmCoords = atm.getBoundingClientRect(); // 
+  let billCoords = bill.getBoundingClientRect();
+  
+  let billLeftTopCorner = {"x" : billCoords.x, "y" : billCoords.y};
+  let billRightTopCorner = {"x" : billCoords.x + billCoords.width, "y" : billCoords.y };
+  
+  let atmLeftTopCorner = {"x" : atmCoords.x, "y" : atmCoords.y};
+  let atmRightTopCorner = {"x" : atmCoords.x + atmCoords.width, "y" : atmCoords.y};
+  let atmLeftBottomCorner = {"x" : atmCoords.x, "y" : atmCoords.y + atmCoords.height/3 };
+  
+  if (billLeftTopCorner.x > atmLeftTopCorner.x
+      && billRightTopCorner.x < atmRightTopCorner.x
+      && billLeftTopCorner.y > atmLeftTopCorner.y
+      && billLeftTopCorner.y < atmLeftBottomCorner.y
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+ 
+  // -------------СДАЧА -----------------------
+  let changeButton = document.querySelector(".change-btn");
+/*  changeButton.onclick = function () {
+    takeChange();   --- как вариант через обертку
+  }*/ 
 
+changeButton.onclick = takeChange;  // получение сдачи при нажатии на кнопку
 
+function takeChange() {
+  tossCoin("10");
+}
 
+function tossCoin(cost) {
+  let changeBox = document.querySelector(".change-box"); 
+  changeBox.style.position = "relative";
+  let changeBoxCoords = changeBox.getBoundingClientRect();
+  let randomWidth = getRandomInt(0, changeBoxCoords.width - 50); // - 50 добавили, чтоб монетки не выходили за границы лотка для монет
+  let randomHeight = getRandomInt(0, changeBoxCoords.height - 50);
+  console.log(randomWidth, randomHeight);
+  
+  let coin = document.createElement("img");
+  coin.setAttribute('src', 'img/10rub.png');
+  coin.style.width = "50px";
+  coin.style.height = "50px";
+  coin.style.position = "absolute";
 
+  
+  
+  changeBox.append(coin); //append - прикрепить в конце элемента*/ 
+  coin.style.top = randomHeight + "px";
+  coin.style.left = randomWidth + "px";
+  
+  //changeBox.prepend(coin); //prepend - прикрепить в начале элемента
+ // changeBox.before(coin); //прикрепить перед элементом
+ // changeBox.after(coin); //прикрепить после элемента
+ // changeBox.replaceWith(coin); // заменяет  элемент
+ 
+}
 
-
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
 
 
 
